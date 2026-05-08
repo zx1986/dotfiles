99d3c1d96ea141259a0ac16313442c994d7530a3
張旭 <zx1986@gmail.com>
2026-05-09 01:54:17 +0800

    fix: add fzf to macOS brew installation list

--- tests/suite_macos.sh
+++ tests/suite_macos.sh
@@ -1,3 +1,4 @@
 echo ">>> Running macOS Suite..."
 check "Has osxkeychain in .gitconfig" "grep -q 'osxkeychain' \$TMP_HOME/.gitconfig"
 check "Brew commands in install script" "grep -q 'brew install' \$TMP_HOME/00_install_packages.sh"
+check "fzf in brew install list" "grep -q 'brew install.*fzf' \$TMP_HOME/00_install_packages.sh"
