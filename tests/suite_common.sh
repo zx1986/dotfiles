2812c8b33868105d7e514da26476f1003c06e9b7
張旭 <zx1986@gmail.com>
2026-05-09 02:04:50 +0800

    test: add verification for fzf dynamic detection

--- tests/suite_common.sh
+++ tests/suite_common.sh
@@ -2,5 +2,6 @@ echo ">>> Running Common Suite..."
 check ".gitconfig rendered" "[[ -f \$TMP_HOME/.gitconfig ]]"
 check ".gitmessage rendered" "[[ -f \$TMP_HOME/.gitmessage ]]"
 check ".zshrc rendered" "[[ -f \$TMP_HOME/.zshrc ]]"
+check ".zshrc contains fzf_shell_paths" "grep -q 'fzf_shell_paths' \$TMP_HOME/.zshrc"
 # Add some checks for directories we expect to exist
 check ".tmux.conf.local exists" "[[ -f \$TMP_HOME/.tmux.conf.local ]]"
