7b6ffea74b9c1ff6bcd408e5619c2d4819b771e3
張旭 <zx1986@gmail.com>
2026-05-26 10:45:22 +0800

    test: add verification for font installation script

--- tests/suite_linux.sh
+++ tests/suite_linux.sh
@@ -1,3 +1,4 @@
 echo ">>> Running Linux Suite..."
 check "No osxkeychain in .gitconfig" "! grep -q 'osxkeychain' \$TMP_HOME/.gitconfig"
 check "APT commands in install script" "grep -q 'apt-get install' \$TMP_HOME/00_install_packages.sh"
+check "Font installation script rendered" "grep -q 'JetBrainsMono' \$TMP_HOME/after_install_fonts.sh"
