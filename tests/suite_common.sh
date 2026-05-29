be2363455cf8ae62b5cd1fdb54c7f2fe1118babd
張旭 <zx1986@gmail.com>
2026-05-29 17:08:22 +0800

    feat: add run_once_after script for tmux plugin installation

--- tests/suite_common.sh
+++ tests/suite_common.sh
@@ -5,3 +5,4 @@ check ".zshrc rendered" "[[ -f \$TMP_HOME/.zshrc ]]"
 check ".zshrc contains fzf_shell_paths" "grep -q 'fzf_shell_paths' \$TMP_HOME/.zshrc"
 # Add some checks for directories we expect to exist
 check ".tmux.conf.local exists" "[[ -f \$TMP_HOME/.tmux.conf.local ]]"
+check "tmux plugin installation script rendered" "[[ -f \$TMP_HOME/after_30_install_tmux_plugins.sh ]]"
